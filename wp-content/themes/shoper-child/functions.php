4baf6cf4fecfe47169c94edebd929e63004cd4e9
satyam1198 <86950390+satyam1198@users.noreply.github.com>
2024-03-02 00:03:46 +0530

    commit wp-content

---
+++ wp-content/themes/shoper-child/functions.php
@@ -0,0 +1,8 @@
+<?php
+// Your code to enqueue parent theme styles
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+?>
